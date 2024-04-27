7c7c338229d794bed7df93e94a6503781df75cb5
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-04-27 20:04:43 -0300

    Punto 1 terminado

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,25 @@
 module Library where
 import PdePreludat
+import Data.List (intersect)
 
-doble :: Number -> Number
-doble numero = numero + numero
+type Cancion = String
+data Artista = UnArtista {
+    nombre :: String,
+    canciones :: [Cancion]
+} deriving Show
+
+fitito :: Artista
+fitito = UnArtista "Fitito Paez" ["11 y 6", "El amor despues del amor", "Mariposa Tecknicolor"]
+
+calamardo :: Artista
+calamardo = UnArtista "Andres Calamardo" ["Flaca", "Sin Documentos", "Tuyo siempre"]
+
+paty :: Artista
+paty = UnArtista "Taylor Paty" ["Shake It Off", "Lover"]
+
+--Punto 1
+letrasMinusculas :: Cancion -> Number
+letrasMinusculas cancion = length (intersect cancion ['a'..'z'])
+
+saberCalificacion :: Cancion -> Number
+saberCalificacion = (10+).letrasMinusculas
